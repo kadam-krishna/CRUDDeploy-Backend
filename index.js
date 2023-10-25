ce2d8c19e54d31819bc96d05ec71e8eafcd38259
Krishna <krishnakadamwork@gmail.com>
2023-10-25 17:15:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const studentRoute = require('./controller/studentRoute');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+mongoose.set("strictQuery",true);   
+mongoose.connect("mongodb+srv://test:12345@cluster0.ezxizfs.mongodb.net/schooldb");
+var db = mongoose.connection;
+db.on("open",()=>console.log("Connected to DB"));
+db.on("error",()=>console.log("Error occurred"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use("/studentRoute",studentRoute);
+app.listen(4000,()=>
+{
+    console.log("Server connected at 4000");
+})
